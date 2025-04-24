d63f48c82b65d106bc8cc00342525b9d9b7a7172
Daniel <danieloftavares@gmail.com>
2025-04-24 09:32:43 -0300

    Codigo IGA

---
+++ projetos/IGA - Interface Grafica Arduino/InterfaceGrafica/InterfaceGrafica/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace InterfaceGrafica
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtSetpoint;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label lblTemperatura;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtSetpoint = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lblTemperatura = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // txtSetpoint
+            // 
+            this.txtSetpoint.Location = new System.Drawing.Point(40, 37);
+            this.txtSetpoint.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSetpoint.Name = "txtSetpoint";
+            this.txtSetpoint.Size = new System.Drawing.Size(132, 22);
+            this.txtSetpoint.TabIndex = 0;
+            this.txtSetpoint.TextChanged += new System.EventHandler(this.txtSetpoint_TextChanged);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(200, 37);
+            this.btnEnviar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(100, 28);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // lblTemperatura
+            // 
+            this.lblTemperatura.AutoSize = true;
+            this.lblTemperatura.Location = new System.Drawing.Point(40, 86);
+            this.lblTemperatura.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTemperatura.Name = "lblTemperatura";
+            this.lblTemperatura.Size = new System.Drawing.Size(103, 16);
+            this.lblTemperatura.TabIndex = 2;
+            this.lblTemperatura.Text = "Temperatura: ---";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 121);
+            this.Controls.Add(this.lblTemperatura);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtSetpoint);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Controle de Temperatura";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
---
+++ projetos/IGA - Interface Grafica Arduino/InterfaceGrafica/InterfaceGrafica/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace InterfaceGrafica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Configurações da porta serial
+            serialPort1.BaudRate = 9600;
+            serialPort1.PortName = "COM9"; // mudar para a COM onde o arduino esta conectado
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao abrir porta serial: " + ex.Message);
+            }
+        }
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string dados = serialPort1.ReadLine();
+
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    if (dados.Contains("Temperatura Atual"))
+                    {
+                        // Extrai apenas o valor numérico da temperatura
+                        string[] partes = dados.Split(':');
+                        if (partes.Length > 1)
+                        {
+                            string temperaturaStr = partes[1].Replace("C", "").Trim();
+                            lblTemperatura.Text = $"{temperaturaStr} ºC";
+                        }
+                    }
+                    else if (dados.Contains("Novo setpoint"))
+                    {
+                        MessageBox.Show(dados, "Confirmação do Arduino", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao ler da serial: " + ex.Message);
+            }
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                string valor = txtSetpoint.Text.Trim();
+
+                if (float.TryParse(valor, out float temp))
+                {
+                    serialPort1.WriteLine(valor);
+                }
+                else
+                {
+                    MessageBox.Show("Digite um valor numérico válido para o setpoint.", "Erro de entrada", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Porta serial não está aberta!");
+            }
+        }
+
+        private void txt_TextChanged(object sender, EventArgs e) { }
+
+        private void txtSetpoint_TextChanged(object sender, EventArgs e) { }
+    }
+}
